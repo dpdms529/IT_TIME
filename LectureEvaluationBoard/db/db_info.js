61119ae1760f044b0035902d7a3687ebf45669b2
wja0 <dnwkdud99@gmail.com>
2021-01-03 22:25:42 +0900

    create LectureEvaluationBoard/

---
+++ LectureEvaluationBoard/db/db_info.js
@@ -0,0 +1,32 @@
+module.exports = (function () {
+    return {
+        local: {
+            host: 'localhost',
+            port: '3306',
+            user: 'root',
+            password: 'wkdud0219',
+            database: 'IT_TIME'
+        },
+        real: {
+            host: '',
+            port: '',
+            user: '',
+            password: '',
+            database: ''
+        },
+        staging: {
+            host: '',
+            port: '',
+            user: '',
+            password: '',
+            database: ''
+        },
+        dev: {
+            host: '',
+            port: '',
+            user: '',
+            password: '',
+            database: ''
+        }
+    }
+})();
