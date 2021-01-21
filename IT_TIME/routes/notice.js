9a1f2c7156210ff78b39131469b818eef0a8b221
YE-EUN <musiclove529@naver.com>
2021-01-21 22:36:42 +0900

    search function

--- IT_TIME/routes/notice.js
+++ IT_TIME/routes/notice.js
@@ -17,25 +17,29 @@ const getHtml = async (url) => {
 };
 
 router.get('/', function (req, res, next) {
-getHtml(url)
-    .then(html => {
-        let ulList = [];
-        const $ = cheerio.load(html.data);
-        const $bodyList = $("tbody tr");
+    getHtml(url)
+        .then(html => {
+            let ulList = [];
+            const $ = cheerio.load(html.data);
+            const $bodyList = $("tbody tr");
 
-        $bodyList.each(function(i, elem) {
-            ulList[i] = {
-                title: $(this).find('td a strong').text(),
-                url: noticeUrl + $(this).find('td a').attr('href'),
-                date: $(this).find('td._artclTdRdate').text()
-            };
+            $bodyList.each(function(i, elem) {
+                ulList[i] = {
+                    title: $(this).find('td a strong').text(),
+                    url: noticeUrl + $(this).find('td a').attr('href'),
+                    date: $(this).find('td._artclTdRdate').text()
+                };
 
-        });
-        return ulList
-    })
-.then(result => res.render('notice', { title:"공지사항", list : result , length : result.length}));
+            });
+            if(req.query.keyword == null){
+                return ulList;
+            }
+            else{
+                keyword = req.query.keyword;
+                return ulList.filter(item=>item.title.includes(keyword));
+            }
+        })
+        .then(result => res.render('notice', { title:"공지사항", list : result , length : result.length}));
 });
-
-
 
 module.exports = router;
